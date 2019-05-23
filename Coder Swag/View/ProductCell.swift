3b49cbd4290f96046a2e190f186ca517f38555ef
Jay Phillips <jayphillips@icloud.com>
2019-05-23 00:21:10 -0400

    finished lesson-3

---
+++ Coder Swag/View/ProductCell.swift
@@ -0,0 +1,21 @@
+//
+//  ProductCell.swift
+//  Coder Swag
+//
+//  Created by Jay Phillips on 5/23/19.
+//  Copyright © 2019 Jay Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productTitle: UILabel!
+    
+    
+    
+    
+    
+}
